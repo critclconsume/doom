cb5d9deeac5a756b96153aaccc640558bce303bd
critclconsume <takatsukisensei140705@gmail.com>
2026-05-18 16:15:39 +0800

    summer im comming!!!

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -11,10 +11,10 @@
   <aside class="sidebar">
     <div class="sidebar-brand">
       <div class="sidebar-brand-icon">
-        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#E1F5EE" stroke-width="2">
-          <path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"/>
-          <polyline points="9 22 9 12 15 12 15 22"/>
-        </svg>
+<div class="sidebar-brand-icon">
+  <img src="{{ asset('images/pu-logo.png') }}" alt="PU Logo" 
+       style="width:28px; height:28px; object-fit:contain; border-radius:4px;">
+</div>
       </div>
       <div>
         <div class="sidebar-brand-name">Sistem Informasi Fasilitas Umum (SIFU)</div>
